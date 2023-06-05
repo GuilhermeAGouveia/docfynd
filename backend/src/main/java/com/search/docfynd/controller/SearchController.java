5f6a74d7ea8a17e50b6ffe30161b51c43fc4d474
GuilhermeAGouveia <guilhermegouveia.dev@gmail.com>
2023-06-05 10:52:08 -0300

    fix: bug in openapi sources generate

--- backend/src/main/java/com/search/docfynd/controller/SearchController.java
+++ backend/src/main/java/com/search/docfynd/controller/SearchController.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.elasticsearch.search.api.facade.SearchApi;
+
 @Service
-public class SearchController implements SearchAPi{
+public class SearchController implements SearchApi{
 
 }
