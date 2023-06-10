4980ba76b4cb3682dc658d46b670175b417194d2
GuilhermeAGouveia <guilhermegouveia.dev@gmail.com>
2023-06-09 23:11:29 -0300

    fix: key error in resultskeleton

--- frontend/src/components/ResultListSkeleton.tsx
+++ frontend/src/components/ResultListSkeleton.tsx
@@ -5,11 +5,16 @@ export default function ResultListSkeleton() {
   return (
     <SkeletonRoot>
       {/* For variant="text", adjust the height via font-size */}
-      <Skeleton variant="text" sx={{ fontSize: "1.5rem", marginBottom: "10px" }} width={"60%"}/>
+      <Skeleton
+        variant="text"
+        sx={{ fontSize: "1.5rem", marginBottom: "10px" }}
+        width={"60%"}
+      />
 
       {/* For other variants, adjust the size with `width` and `height` */}
       {new Array(10).fill(0).map((_, i) => (
         <Skeleton
+          key={"ResultListSkeleton" + i}
           variant="rounded"
           width={"100%"}
           height={220}
